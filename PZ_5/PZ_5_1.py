991efcb93e16364ba19c0a9ddc953b0bf7cdfa96
NiceKirill <supergaame0805@gmail.com>
2024-11-30 11:25:58 +0300

    Code PZ_5_2

--- PZ_5/PZ_5_1.py
+++ PZ_5/PZ_5_1.py
@@ -1,1 +1,13 @@
 ## Составить функцию, которая напечатает сорок любых символов.
+
+import random
+
+def print_random_chars():
+    # Создаем строку с 40 случайными символами
+    chars = ''.join(random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789') for _ in range(40))
+
+    # Печатаем строку
+    print(chars)
+
+# Вызов функции
+print_random_chars()
